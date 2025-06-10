468b1b000a3379bf277ba440d6ed48a0d00a52e5
Felix Pultar <felix.pultar@alumni.ethz.ch>
2025-06-10 12:31:41 +0200

    simplified type

--- Sources/App/Model/GamePassGameDetailsResponse.swift
+++ Sources/App/Model/GamePassGameDetailsResponse.swift
@@ -8,11 +8,7 @@
 import Foundation
 import XboxKit
 
-public struct GamePassGameDetailsResponse: Equatable, Codable, Sendable {
-    public let game: XboxGame
-    public let availabilityHistory: [Date]
-    public init(game: XboxGame, availabilityHistory: [Date]) {
-        self.game = game
-        self.availabilityHistory = availabilityHistory
-    }
+struct GamePassGameDetailsResponse: Equatable, Codable, Sendable {
+    let game: XboxGame
+    let availabilityHistory: [Date]
 }
